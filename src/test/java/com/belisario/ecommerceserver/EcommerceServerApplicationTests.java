aef7a32329b731f6db92ac469ea293c2361ae6c0
luisbelisario <luisbls77@hotmail.com>
2023-08-30 10:08:25 -0300

    first commit

---
+++ src/test/java/com/belisario/ecommerceserver/EcommerceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belisario.ecommerceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
